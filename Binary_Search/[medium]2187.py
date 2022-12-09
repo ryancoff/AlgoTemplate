29366f7d8b30d3869ecd03e4d2b928e51de79621
HieuNP <HieuNP54@bitbucket.org>
2022-12-09 11:09:09 +0700

    add sol for 2187

---
+++ Binary_Search/[medium]2187.py
@@ -0,0 +1,26 @@
+# 2187. Minimum Time to Complete Trips
+
+class Solution:
+    def minimumTime(self, time: List[int], totalTrips: int) -> int:
+        min_t = min(time) # Might no need
+
+        upperBound = math.ceil(totalTrips*min_t) # total/fastest_velocity
+        # print("upperBound: ",upperBound)
+        left, right = 0, upperBound
+        while left < right:
+            mid = left + (right - left)//2
+            if self.calCurrentTrips(time,mid) < totalTrips:
+                left = mid + 1
+            else:
+                right = mid
+        
+        # print("Left & total:",self.calCurrentTrips(time,left))
+        # print("Left: ", right)
+        if self.calCurrentTrips(time,left-1) >= totalTrips:
+            # print("Here!")
+            return left-1  
+        else:
+            return left
+
+    def calCurrentTrips(self, time, currTime):
+        return sum(currTime//t for t in time)
