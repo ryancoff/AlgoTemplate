9eb86b597e3ab8da88a628ba32bb7be6e821bd26
HieuNP <HieuNP54@bitbucket.org>
2022-12-11 23:22:48 +0700

    add sol for 2271

--- Binary_Search/[medium_redo_rerun]2271.py
+++ Binary_Search/[medium_redo_rerun]2271.py
@@ -1,5 +1,28 @@
 # 2271. Maximum White Tiles Covered by a Carpet (89.30%)
 
+# Two pointer (98.52%)
+class Solution:
+    def maximumWhiteTiles(self, tiles: List[List[int]], carpetLen: int) -> int:
+        tiles.sort()
+        res = 0
+        l, r = 0, 0
+        N = len(tiles)
+        cover = 0
+        while res < carpetLen and r < N:
+            if tiles[l][0] + carpetLen > tiles[r][1]:
+                cover += tiles[r][1] - tiles[r][0] + 1
+                res = max(res, cover)
+                r += 1
+            else:
+                partial = max(0, tiles[l][0] + carpetLen - tiles[r][0])
+                res = max(res, cover + partial)
+                cover -= tiles[l][1] - tiles[l][0] + 1
+                l += 1
+        
+        return res
+
+
+# Binary Seach 
 import bisect
 class Solution:
     def maximumWhiteTiles(self, tiles: List[List[int]], l: int) -> int:
