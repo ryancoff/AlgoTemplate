697949e561919077bc5367780f028062ddecd0f0
HieuNP <HieuNP54@bitbucket.org>
2022-12-20 11:24:27 +0700

    add sol for 1838

--- Sliding_Window/[Medium_nice]1838.py
+++ Sliding_Window/[Medium_nice]1838.py
@@ -1,7 +1,7 @@
 # 1838. Frequency of the Most Frequent Element
 # Guide: https://leetcode.com/problems/frequency-of-the-most-frequent-element/solutions/1175088/c-maximum-sliding-window-cheatsheet-template/?orderBy=most_votes
 
-# Optimized (100%)
+# Optimized (100%) (Non-shrinkable)
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         l = 0
@@ -27,9 +27,21 @@ class Solution:
         for j in range(N):
             sum += nums[j]
             while k < nums[j]*(j - i + 1) - sum:
-                sum -= nums[i]
+                sum -= nums[i] # Update sum = shrink left
                 i += 1
             ans = max(ans,j-i+1)
         return ans
-
     
+# Template 2 (Non-shrinkable) (Faster) (97.90%)
+# GROW = Move Right when valid, and SHIFT = SHRINK = Move Left when invalid
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        i,j,N = 0,0,len(nums)
+        sum = 0
+        for j in range(N):
+            sum += nums[j]
+            if k < nums[j]*(j - i + 1) - sum:
+                sum -= nums[i] # Update sum = shrink left
+                i += 1
+        return j-i+1
